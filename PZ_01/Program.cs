33d0b7ad28b41ce9556b91d9d157af1553e3a44f
user <user@WIN-N1U0GT2AIVJ>
2023-09-09 16:14:50 +0500

    Добавьте файлы проекта.

---
+++ PZ_01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a ");  
+            double a = double.Parse(Console.ReadLine());  //ввод значения с клавиатуры
+
+            Console.WriteLine("Введите число b ");
+            double b = double.Parse(Console.ReadLine());  //ввод значения с клавиатуры
+
+            double c = (Math.PI) / 4; //присваивание переменной значения
+
+            double num1, num2, num3; //делим выражение на две части для удобства работы с ним
+
+            num1 = Math.Pow((a - 1), 1/3);  //вычисления
+
+            num2 = Math.Pow((b + num1), 1/4); //вычисления
+
+            num3 = (Math.Abs(a - b)) * (Math.Pow(Math.Sin (c), 2) + Math.Tan(c)); //вычисления
+
+            double result = num2 / num3;  //создание переменной результата
+            Console.WriteLine("Результат " + result);  //вывод результата на экран
+
+            Console.ReadLine();
+
+        }
+    }
+}
