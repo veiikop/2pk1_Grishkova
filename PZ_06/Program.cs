997f9433842915b30ac8541a59f96af0503f7308
veiikop <haitanirun@mail.ru>
2023-09-27 12:54:13 +0500

    Добавлена шестая практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+
+            // Заполнение массива случайными числами от -50 до 49
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-50, 50);
+            }
+
+            int sumOfNegatives = 0;
+            int minPositive = int.MaxValue;
+
+            // Нахождение суммы отрицательных элементов и минимального положительного элемента
+            foreach (int number in array)
+            {
+                if (number < 0)
+                {
+                    sumOfNegatives += number;
+                }
+                else if (number > 0 && number < minPositive)
+                {
+                    minPositive = number;
+                }
+            }
+
+            // Прибавление минимального положительного элемента к сумме отрицательных элементов, если она меньше -100
+            if (sumOfNegatives < -100)
+            {
+                sumOfNegatives += minPositive;
+            }
+
+            Console.WriteLine("Сумма отрицательных элементов: " + sumOfNegatives);
+        }
+    }
+}
