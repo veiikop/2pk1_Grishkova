e109ba4c84d1c0b333eb539801fc349eef00e659
veiikop <haitanirun@mail.ru>
2023-11-23 19:59:57 +0500

    добавлена десятая практика

---
+++ PZ-10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символы ");
+            string text = Console.ReadLine();
+            int length = text.Length;
+            if (length % 2 == 0)
+            {
+                // количество символов чётное, найдем два символа посередине
+                int middle = length / 2;
+                char middleChar1 = text[middle - 1];
+                char middleChar2 = text[middle];
+                Console.WriteLine("Символы посередине: {0}, {1}", middleChar1, middleChar2);
+            }
+            else
+            {
+                // количество символов нечётное, найдем символ посередине
+                int middle = length / 2;
+                char middleChar = text[middle];
+                Console.WriteLine("Символ посередине: {0}", middleChar);
+            }
+        }
+    }
+}
