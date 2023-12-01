649326b68ae5e7c669fc6208a7cd571e21991c93
veiikop <haitanirun@mail.ru>
2023-12-01 22:00:54 +0500

    Добавлена одиннадцатая практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void GetMax(int[] numbers, out int maxValue)
+        {
+            maxValue = int.MinValue;
+            foreach (int number in numbers)
+            {
+                if (number > maxValue)
+                {
+                    maxValue = number;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 21, 7, 11, 89, 4 };
+            int maxValue;
+            GetMax(numbers, out maxValue);
+            Console.WriteLine("Max value: " + maxValue);
+        }
+    }
+}
