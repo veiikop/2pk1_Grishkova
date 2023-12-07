f3e95b59ccd5d81dd7f6983d1b976a3ce6dccd9d
veiikop <haitanirun@mail.ru>
2023-12-07 12:47:13 +0500

    добавлена пятнадцатая практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к каталогу:");
+            string path = Console.ReadLine();
+
+            try
+            {
+                // Создаем подкаталог TEMP
+                string tempPath = Path.Combine(path, "TEMP");
+                Directory.CreateDirectory(tempPath);
+
+                // Получаем список файлов в каталоге
+                string[] files = Directory.GetFiles(path);
+
+                // Переносим файлы в TEMP
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destFile = Path.Combine(tempPath, fileName);
+                    File.Move(file, destFile);
+                }
+
+                Console.WriteLine("Файлы успешно перемещены в папку TEMP");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
